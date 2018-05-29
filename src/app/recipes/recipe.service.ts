95e211871ba27bc1c3d3f3605f66f15320a0d024
Serhiy K <mrlanu@gmail.com>
2018-05-29 09:45:09 -0500

    Ingredients to Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
@@ -8,10 +9,14 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('Pizza',
       'Test',
-      'https://www.maxpixel.net/static/photo/1x/Comisa-Healthy-Tomato-Recipe-2930786.jpg'),
+      'https://www.maxpixel.net/static/photo/1x/Comisa-Healthy-Tomato-Recipe-2930786.jpg',
+      [new Ingredient('Tomatoes', 5),
+            new Ingredient('Mushrooms', 4)]),
     new Recipe('Ribs',
       'Test 2',
-      'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+      'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg',
+      [new Ingredient('Ribs', 2),
+            new Ingredient('Sugar', 3)])
   ];
 
   getRecipes() {

ce7a089de08f41d6528b3ee4f90530a74904260b
Serhiy K <mrlanu@gmail.com>
2018-09-30 19:30:59 -0500

    SignInComponent

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,4 +9,14 @@ export class AuthService {
         console.log(error);
       });
   }
+
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -8,9 +9,13 @@ import {NgForm} from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSignIn(form: NgForm) {
+    this.authService.signInUser(form.value.email, form.value.password);
   }
 
 }
